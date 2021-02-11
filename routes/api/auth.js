08915b0f88dc977f69a0863226ba2d25b9c9e746
Giles Smith <gilesomofa@yahoo.com>
2021-02-11 17:12:08 -0500

    updating routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,96 +1,101 @@
 const express = require('express');
 const router = express.Router();
 
-//bring in the middle ware 
-const auth = require('../../middleware/auth'); 
+//bring in the middle ware
+const auth = require('../../middleware/auth');
 
-//bring in the User model from mongo via mongoose 
+//bring in the User model from mongo via mongoose
 const User = require('../../models/User');
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config'); 
-const {check, validationResult} = require("express-validator");
- 
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
 // Get request   to api/auth
 // Description: Authenticate user and get token
 // Public to make request to use on private routes
 
 //by bringing in the auth middleware and placing as the second parameter, this route becomes protected
 
-router.get('/', auth, async (req, res) =>{
-    try {
-        //asynchronously query route for authenticated 'user' and exclude password in
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);   
-        console.error(error.message);
-        res.status(500).send('Ser ver Error ')
-}   catch(err){
-
-}
+router.get('/', auth, async (req, res) => {
+	try {
+		//asynchronously query route for authenticated 'user' and exclude password in
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(error.message);
+		res.status(500).send('Server Error ');
+	}
 });
 
-//post req to api/auth
-//authenticate user and get token
-//public
+//Post req to api/auth
+//Authenticate user and get token
+//Public
 
-router.post('/', [
-    //this code is from validator package and handles validation of credentials
-    check('email', 'Please enter a valid email address').isEmail(),
-    check('password', 'Password is required').exists()
-],
-async (req, res) => {
-    //represents the object of data that will be sent to this route(needs middleware to parse)
-const errors = validationResult(req);
-if(!errors.isEmpty()){
-    return res.status(400).json({ errors: errors.array() });
-}   
+router.post(
+	'/',
+	[
+		//this code is from validator package and handles validation of credentials
+		check('email', 'Please enter a valid email address').isEmail(),
+		check('password', 'Password is required').exists(),
+	],
+	async (req, res) => {
+		//represents the object of data that will be sent to this route(needs middleware to parse)
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-const { email, password  } = req.body;
+		const { email, password } = req.body;
 
-//verify that user's credentials arent' being used
+		//verify that user's credentials arent' being used
 
-try {
-    let user = await User.findOne({ email })
-    //check to see if not user
-    if(!user) {
-        //send back invalid credential message
-       return res.status(400).json( { errors: [ { msg: 'Invalid credentials' } ] } );
-    } 
+		try {
+			let user = await User.findOne({ email });
+			//check to see if not user
+			if (!user) {
+				//send back invalid credential message
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid credentials' }] });
+			}
 
-//will match user to password using bcrypt method 'compare'
-//create variable to hold value from bcrypt.compare()
-//bcrypt.compare takes in plain text password and encrypted user.password that is stored in the user 
-//object coming from Mongo
+			//will match user to password using bcrypt method 'compare'
+			//create variable to hold value from bcrypt.compare()
+			//bcrypt.compare takes in plain text password and encrypted user.password that is stored in the user
+			//object coming from Mongo
 
-const isMatch = await bcrypt.compare(password, user.password);
+			const isMatch = await bcrypt.compare(password, user.password);
 
-//using !isMatch return message stating credentials are invalid
-if(!isMatch){
-    return res.status(400).json( { errors: [ { msg: 'Invalid credentials' }  ] } );
-}
+			//using !isMatch return message stating credentials are invalid
+			if (!isMatch) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid credentials' }] });
+			}
 
- 
-//return Jwt 
-const payload = {
-    user: {
-        id: user.id
-    }
-}
-jwt.sign(payload,
-        config.get('jwtSecret'),
-        {  expiresIn:360000 },
-        (err, token) => {
-            if(err) throw err;
-            //this token will be used as headers in secure routes
-            res.json({ token });
-        } 
+			//return Jwt
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					//this token will be used as headers in secure routes
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send('Server error');
+		}
+	}
 );
-}   catch(err){
-    console.log(err.message);
-    res.status(500).send('Server error');
-
-}});
-    
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,23 +7,24 @@ const User = require('../../models/User');
 
 // Get api/profile/me
 // get current users profile
-// private 
+// private
 
+//this route will get the user's profile
+router.get('/me', auth, async (req, res) => {
+	try {
+		//write query to Mongo to get values of user to be used to create user value in profile
+		const profile = await Profile.findOne({
+			user: req.user.id,
+		}).populate('user', ['name', 'avatar']);
+		if (!profile) {
+			return res.status(400).json({ msg: 'There is no profile for user' });
+		}
 
-//this route will be for the user's profile 
-router.get('/me', auth,  async (req, res) => {
-    try{
-        //write query to Mongo to get values of user to be used to create user value in profile 
-        const profile = await  Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']); 
-        if(!profile){
-            return res.status(400).json({ msg: 'There is no profile for user' });
-        }
-
-        res.json(profile);
-    }   catch(err){
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }  
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // Post api/profile
@@ -31,159 +32,151 @@ router.get('/me', auth,  async (req, res) => {
 // private
 
 router.post(
-    '/', 
-    //the below brings in auth middleware to secure route and following that
-    //is the express validator method 'check' which validates user profile skills and status
-    [
-   auth, 
-        [
-    check('status', 'Status is required')
-    .not()
-    .isEmpty(),
-    check('skills', 'Skills is required')
-]
-],
-async (req, res) => {
-    //since this route will be able to create new profile, it must be secured
-    //with validationResult from express-validator
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json( { errors: errors.array() });
-    }
-    const{
-        company,
-        website,
-        location,
-        bio,
-        status,
-        githubusername,
-        skills,
-        youtube,
-        facebook,
-        twitter,
-        instagram,
-        linkedin
-     } = req.body
-     
-     //Build profile object
+	'/',
+	//the below brings in auth middleware to secure route and following that
+	//is the express validator method 'check' which validates user profile skills and status
+	[
+		auth,
+		[
+			check('status', 'Status is required').not().isEmpty(),
+			check('skills', 'Skills is required'),
+		],
+	],
+	async (req, res) => {
+		//since this route will be able to create new profile, it must be secured
+		//with validationResult from express-validator
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const {
+			company,
+			website,
+			location,
+			bio,
+			status,
+			githubusername,
+			skills,
+			youtube,
+			facebook,
+			twitter,
+			instagram,
+			linkedin,
+		} = req.body;
 
-     const profileFields = {};
+		//Build profile object
 
-     //allow access to the user through req.user.id (which has token attached)
-     profileFields.user = req.user.id;
+		const profileFields = {};
 
-     if(company) profileFields.company = company; 
-     if(website) profileFields.website = website;
-     if(location) profileFields.location = location;
-     if(bio) profileFields.bio = bio;
-     if(status) profileFields.status = status;
-     if(githubusername) profileFields.githubusername = githubusername; 
-     
-     // Since skills has multiple items, it must be received with the ability to
-     //store an array, hence the following code
-     if(skills) {
-     profileFields.skills = skills.split(',').map(skill => skill.trim());
+		//allow access to the user through req.user.id (which has token attached)
+		profileFields.user = req.user.id;
 
-    } 
-    console.log(profileFields.skills)
-    res.send('hello')
-    //Initialize social object 
-    profileFields.social = {}
-    if (youtube) profileFields.social.youtube = youtube;
-    if (twitter) profileFields.social.twitter = twitter;
-    if (facebook) profileFields.social.facebook = facebook;
-    if (linkedin) profileFields.social.linkedin = linkedin;
-    if (instagram) profileFields.social.instagram = instagram;
+		if (company) profileFields.company = company;
+		if (website) profileFields.website = website;
+		if (location) profileFields.location = location;
+		if (bio) profileFields.bio = bio;
+		if (status) profileFields.status = status;
+		if (githubusername) profileFields.githubusername = githubusername;
 
-    try {
+		// Since skills has multiple items, it must be received with the ability to
+		//store an array, hence the following code
+		if (skills) {
+			profileFields.skills = skills.split(',').map((skill) => skill.trim());
+		}
+		res.send(profileFields);
+		//Initialize social object
+		profileFields.social = {};
+		if (youtube) profileFields.social.youtube = youtube;
+		if (twitter) profileFields.social.twitter = twitter;
+		if (facebook) profileFields.social.facebook = facebook;
+		if (linkedin) profileFields.social.linkedin = linkedin;
+		if (instagram) profileFields.social.instagram = instagram;
 
-        //Remember, when using mongoose methods we get a promise
-        //Hence  'async'- 'await' - don't forget!!
-        //Also, note findOneAndUpdate mongoose method and syntax
+		try {
+			//Remember, when using mongoose methods we get a promise
+			//Hence  'async'- 'await' - don't forget!!
+			//Also, note findOneAndUpdate mongoose method and syntax
 
-        let profile = await Profile.findOne({ user: req.user.id });
-
-        if(profile) {
+			let profile = await Profile.findOne({ user: req.user.id });
+                
             //update profile if one exists
 
-            profile = await Profile.findOneAndUpdate(
-                { user: req.user.id },
-                { set: profileFields },
-                { new: true }
-                );
-                return res.json(profile);
-        }
-        //  If there is no profile Create a new one
+			if (profile) {
+                //update
+				profile = await Profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+                    { new: true, upsert: true, setDefaultsOnInsert: true }
+				);
+				return res.json(profile);
+			}
+			// Create a new one
 
-        profile = new Profile(profileFields);
-
-        await profile.save()
-        res.json(profile)
-
-    } catch(err) {
-        console.error(err.message);  
-        res.status(500).send('Server Error');
-    }
-}
-); 
+			profile = new Profile(profileFields); 
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 // @route  Get api/profile
 // @description  Get all profiles
 // @access Public
 
-router.get('/', async (req,res) =>{
-    try {
-        const profiles = await Profile.find().populate('user', [ 'name', 'avatar']);
-        res.json(profiles);  
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error')
-    }  
+router.get('/', async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+		res.json(profiles);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
-// @route  Get api/profile/user/:user_id  
+// @route  Get api/profile/user/:user_id
 // @description  Get profile by user ID
-// @access Public  
+// @access Public
 
-router.get('/user/:user_id', async (req,res) =>{
-    try {
-        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', [ 'name', 'avatar']);
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id,
+		}).populate('user', ['name', 'avatar']);
 
-        if(!profile) return res.stautus(400).json({ msg: 'Profile not found'});
+		if (!profile) return res.stautus(400).json({ msg: 'Profile not found' });
 
-        res.json(profile);  
-    } catch (err) {
-        console.error(err.message);
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
 
-        if(err.kind == 'ObjectId'){
-            return res.stautus(400).json({ msg: 'Profile not found'});
-        }
-        res.status(500).send('Server Error')
-    }  
+		if (err.kind == 'ObjectId') {
+			return res.stautus(400).json({ msg: 'Profile not found' });
+		}
+		res.status(500).send('Server Error');
+	}
 });
 // @route  Delete api/profile
 // @description  Delete  profile, user, and posts
 // @access Private
 
-router.delete('/', auth, async (req,res) =>{
-    try {
-        //@todo - remove users posts
+router.delete('/', auth, async (req, res) => {
+	try {
+		//@todo - remove users posts
 
-        //remove profile
-         await Profile.findOneAndRemove( {user: req.user.id });
-         //Remove user
-         await User.findOneAndRemove( {_id: req.user.id });
-        res.json('user removed')
-        res.json(profiles);  
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error')
-    }  
+		//remove profile
+		await Profile.findOneAndRemove({ user: req.user.id });
+		//Remove user
+		await User.findOneAndRemove({ _id: req.user.id });
+		res.json({ msg: 'user removed' });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route  Put api/profile/experience
 // @description  Add profile experience
 // @access Private
-
 
 module.exports = router;
